e4492180e2bd6474cebccd6830acb0552793c6e1
sonictrain <nicola.brucoli92@gmail.com>
2024-03-23 18:02:05 +0000

    adding placeholder component to display Tasks

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,9 @@
+import { Typography } from "@material-tailwind/react";
+
+const ToDoList = () => {
+    return (
+        <Typography variant="h3">Task List</Typography>
+    )
+};
+
+export default ToDoList;
