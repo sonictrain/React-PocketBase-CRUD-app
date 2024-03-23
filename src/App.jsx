d7eaac785160f547d33e2bf67e33737f054ca5c1
sonictrain <nicola.brucoli92@gmail.com>
2024-03-23 18:02:46 +0000

    adding useEffect hook to fetch from PocketBase SQL Table

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,18 @@ import {
   ButtonGroup,
   Button
 } from "@material-tailwind/react";
+import { useEffect } from 'react';
+import { client } from './lib/pocketbase';
 
 const App = () => {
+
+  useEffect(() => {
+    client
+    .collection("tasks")
+    .getFullList()
+    .then(res => console.log(res));
+  }, [])
+
   return (
     <div className="flex flex-row justify-between p-20">
       <Typography variant="h1">Task App</Typography>
