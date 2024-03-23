6360c569875ff4a508f9545837011c62315d494b
sonictrain <nicola.brucoli92@gmail.com>
2024-03-23 15:54:01 +0000

    addinng boilerplate frontend code bootstrapped with Vite. Added Tailwind & Material Tailwind configuration

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import {
+  Typography,
+  ButtonGroup,
+  Button
+} from "@material-tailwind/react";
+
+const App = () => {
+  return (
+    <div className="flex flex-row justify-between p-20">
+      <Typography variant="h1">Task App</Typography>
+      <ButtonGroup>
+        <Button>Sign In</Button>
+        <Button>Sign Up</Button>
+      </ButtonGroup>
+    </div>
+    )
+}
+
+export default App;
