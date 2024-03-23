000969f214974649e05dabcfe8e080f9e3c0dd35
sonictrain <nicola.brucoli92@gmail.com>
2024-03-23 19:19:21 +0000

    Added fetch call and map rendering in a card component provided by material tailwind

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,8 +1,47 @@
-import { Typography } from "@material-tailwind/react";
+import { useEffect, useState } from 'react';
+import { client } from '../lib/pocketbase';
+import {
+    Card,
+    CardBody,
+    CardFooter,
+    Typography,
+    Button,
+    Chip
+} from "@material-tailwind/react";
 
 const ToDoList = () => {
+
+    const [tasks, setTasks] = useState([])
+
+    useEffect(() => {
+        client
+            .collection("tasks")
+            .getFullList()
+            .then(res => setTasks(res));
+    }, [])
+
     return (
-        <Typography variant="h3">Task List</Typography>
+        <>
+            <Typography variant="h3">Task List</Typography>
+            {
+                tasks.map((t) => (
+                    <Card key={t.id} className="mt-6 w-96 items-start">
+                        <Chip value={t.id} variant="ghost" size="sm" className="rounded-full mt-5 ms-5" />
+                        <CardBody className="items-start pt-4">
+                            <Typography variant="h5" color="blue-gray" className="mb-2">
+                                {t.title}
+                            </Typography>
+                            <Typography>
+                                {t.description}
+                            </Typography>
+                        </CardBody>
+                        <CardFooter className="pt-0 w-full">
+                            <Button fullWidth disabled={t.isCompleted}>Mark as completed</Button>
+                        </CardFooter>
+                    </Card>
+                ))
+            }
+        </>
     )
 };
 
