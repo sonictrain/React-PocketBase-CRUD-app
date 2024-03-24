543c348a96b9502cad0f92afde80adaccb7ad2d6
sonictrain <nicola.brucoli92@gmail.com>
2024-03-24 01:50:39 +0000

    Added getTasks method to the pocketbase client function so that it can be called from any directory

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { client } from '../lib/pocketbase';
+import { getTasks } from '../lib/pocketbase';
 import {
     Card,
     CardBody,
@@ -14,34 +14,33 @@ const ToDoList = () => {
     const [tasks, setTasks] = useState([])
 
     useEffect(() => {
-        client
-            .collection("tasks")
-            .getFullList()
-            .then(res => setTasks(res));
+        getTasks().then(res => setTasks(res));
     }, [])
 
     return (
-        <>
+        <div className='flex flex-col gap-10'>
             <Typography variant="h3">Task List</Typography>
-            {
-                tasks.map((t) => (
-                    <Card key={t.id} className="mt-6 w-96 items-start">
-                        <Chip value={t.id} variant="ghost" size="sm" className="rounded-full mt-5 ms-5" />
-                        <CardBody className="items-start pt-4">
-                            <Typography variant="h5" color="blue-gray" className="mb-2">
-                                {t.title}
-                            </Typography>
-                            <Typography>
-                                {t.description}
-                            </Typography>
-                        </CardBody>
-                        <CardFooter className="pt-0 w-full">
-                            <Button fullWidth disabled={t.isCompleted}>Mark as completed</Button>
-                        </CardFooter>
-                    </Card>
-                ))
-            }
-        </>
+            <div className='grid grid-cols-4 gap-4'>
+                {
+                    tasks.map((t) => (
+                        <Card key={t.id} className="mt-6 items-start">
+                            <Chip value={t.id} variant="ghost" size="sm" className="rounded-full mt-5 ms-5" />
+                            <CardBody className="items-start pt-4">
+                                <Typography variant="h5" color="blue-gray" className="mb-2">
+                                    {t.title}
+                                </Typography>
+                                <Typography>
+                                    {t.description}
+                                </Typography>
+                            </CardBody>
+                            <CardFooter className="pt-0 w-full">
+                                <Button fullWidth disabled={t.isCompleted}>Mark as completed</Button>
+                            </CardFooter>
+                        </Card>
+                    ))
+                }
+            </div>
+        </div>
     )
 };
 
--- src/lib/pocketbase.js
+++ src/lib/pocketbase.js
@@ -4,3 +4,7 @@ const url = 'https://sonictrain-app-dark-surf-6793.fly.dev';
 export const client = new PocketBase(url);
 
 client.autoCancellation(false);
+
+export const getTasks = async () => {
+    return await client.collection("tasks").getFullList();
+};
