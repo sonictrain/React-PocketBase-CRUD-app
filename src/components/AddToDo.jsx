e9f915e5881f42a38939d687c8d8eb5b61d8058c
sonictrain <nicola.brucoli92@gmail.com>
2024-03-24 01:49:22 +0000

    Created Add To Do button component

---
+++ src/components/AddToDo.jsx
@@ -0,0 +1,16 @@
+import { IconButton } from "@material-tailwind/react";
+
+const AddToDo = () => {
+
+    return (
+        <div className="fixed bottom-0 right-0">
+            <IconButton
+                size="lg"
+                className="m-20 rounded-full">
+                <i className="fa-solid fa-plus" />
+            </IconButton>
+        </div>
+    )
+};
+
+export default AddToDo;
