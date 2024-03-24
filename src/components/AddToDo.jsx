14589ae93a18e22fef9062b604901471456e0066
sonictrain <nicola.brucoli92@gmail.com>
2024-03-24 15:42:43 +0000

    Adding modal form for creating new Task, and setting up the states to handle the input change

--- src/components/AddToDo.jsx
+++ src/components/AddToDo.jsx
@@ -1,14 +1,57 @@
-import { IconButton } from "@material-tailwind/react";
+import { React, useState } from 'react';
+import {
+    IconButton,
+    Button,
+    Dialog,
+    Card,
+    CardBody,
+    CardFooter,
+    Typography,
+    Input,
+    Textarea
+} from "@material-tailwind/react";
 
 const AddToDo = () => {
 
+    const [newTaskData, setNewTaskData] = useState({
+        title: null,
+        descriotion: null,
+    });
+    const [open, setOpen] = useState(false);
+
+    const handleOpen = () => {
+        setOpen((open) => !open);
+    };
+
     return (
         <div className="fixed bottom-0 right-0">
+
             <IconButton
+                onClick={handleOpen}
                 size="lg"
                 className="m-8 md:m-12 lg:m-20 rounded-full">
                 <i className="fa-solid fa-plus" />
             </IconButton>
+
+            <Dialog size="xs" open={open} handler={handleOpen} className="bg-transparent shadow-none">
+
+                <Card className="mx-auto w-full max-w-[24rem]">
+                    <CardBody className="flex flex-col gap-4">
+                        <Typography variant="h4" color="blue-gray">
+                            Add a new Task
+                        </Typography>
+                        <Input label="Title" size="lg" required/>
+                        <Textarea label="Description" size="lg" required/>
+                    </CardBody>
+                    <CardFooter className="pt-0">
+                        <Button variant="gradient" onClick={handleOpen} fullWidth>
+                            Save task
+                        </Button>
+                    </CardFooter>
+                </Card>
+
+            </Dialog>
+
         </div>
     )
 };
