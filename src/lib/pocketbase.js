5a8ad1cf4b48374e32470d4a9b1d7463b356d8f5
sonictrain <nicola.brucoli92@gmail.com>
2024-03-25 19:31:30 +0000

    replacing explicit URL with Environment variable

--- src/lib/pocketbase.js
+++ src/lib/pocketbase.js
@@ -1,6 +1,6 @@
 import PocketBase from 'pocketbase';
 
-const url = 'https://sonictrain-app-dark-surf-6793.fly.dev';
+const url = import.meta.env.VITE_POCKETBASE_URL;
 export const client = new PocketBase(url);
 
 client.autoCancellation(false);
@@ -12,4 +12,10 @@ export const getTasks = async () => {
 export const createTask = async (t, d) => {
     const data = { title: t, description: d }
     await client.collection("tasks").create(data);
+}
+
+export const deleteTask = async (id, confirm) => {
+    if (confirm) {
+        await client.collection("tasks").delete(id);
+    }
 }
