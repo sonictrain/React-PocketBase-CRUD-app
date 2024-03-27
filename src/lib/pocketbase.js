2bc144938b611669dc69b34695050d2290c18e40
sonictrain <nicola.brucoli92@gmail.com>
2024-03-27 16:42:28 +0000

    Added method to sign in, sign out, and sign up. Updated old methods to pass the user id in the pb call

--- src/lib/pocketbase.js
+++ src/lib/pocketbase.js
@@ -2,6 +2,7 @@ import PocketBase from 'pocketbase';
 
 const url = import.meta.env.VITE_POCKETBASE_URL;
 export const client = new PocketBase(url);
+export const isUserValid = client.authStore.isValid;
 
 client.autoCancellation(false);
 
@@ -10,27 +11,50 @@ export const getTasks = async () => {
 };
 
 export const createTask = async (t, d) => {
-    const data = { title: t, description: d }
+    const data = {
+        title: t,
+        description: d,
+        user: client.authStore.model.id
+    };
     await client.collection("tasks").create(data);
 };
 
 export const deleteTask = async (id, confirm) => {
     if (confirm) {
         await client.collection("tasks").delete(id);
-    }
+    };
 };
 
 export const updateTask = async (id, title, description) => {
     const data = {
         title: title,
-        description: description
+        description: description,
+        user: client.authStore.model.id
     };
     await client.collection("tasks").update(id, data);
 };
 
 export const toggleTask = async (id, isCompleted) => {
     const data = {
-        isCompleted: isCompleted
+        isCompleted: isCompleted,
+        user: client.authStore.model.id
     };
     await client.collection("tasks").update(id, data);
+};
+
+export const signIn = async (username, pwd) => {
+    await client.collection("users").authWithPassword(username, pwd);
+};
+
+export const signOut = () => {
+    client.authStore.clear;
+};
+
+export const signUp = () => {
+    const data = {
+        username: username,
+        password: password,
+        passwordConfirm: passwordConfirm,
+    };
+    client.collection("users").create(data);
 };
