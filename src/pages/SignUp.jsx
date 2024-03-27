f9e16dc31547fb050b1271896b13113eb5002f66
sonictrain <nicola.brucoli92@gmail.com>
2024-03-27 16:45:27 +0000

    Created placeholder components for Sign In and Sign Up

---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,11 @@
+import { Typography } from "@material-tailwind/react";
+
+const SignUp = () => {
+    return (
+        <Typography variant="h2" color="blue" textGradient>
+            Sign Up
+        </Typography>
+    )
+}
+
+export default SignUp;
