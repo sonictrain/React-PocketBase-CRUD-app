2dec046546d9ccc009d111afe7c9123480e95779
sonictrain <nicola.brucoli92@gmail.com>
2024-03-28 23:50:34 +0000

    Reworked placeholder Sign In component to handle input, submission, basic validation with alert (to improve) and conditional rendering based on the isLoggedIn status

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,16 +1,62 @@
+import { useState } from 'react'
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 import {
     Card,
+    Alert,
     CardHeader,
     CardBody,
     CardFooter,
     Typography,
     Input,
-    Checkbox,
     Button,
-  } from "@material-tailwind/react";
+} from "@material-tailwind/react";
+import { client, signIn } from '../lib/pocketbase';
 
 const SignIn = () => {
+
+    const [userData, setUserData] = useState({
+        email: '',
+        password: ''
+    });
+    const navigate = useNavigate()
+
+    const [openAlert, setOpenAlert] = useState(false);
+    const [alertMsg, setAlertMsg] = useState('');
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setUserData({
+            ...userData,
+            [name]: value
+        })
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setOpenAlert(false);
+        const { email, password } = userData;
+
+        if (email.length > 0 && password.length > 0) {
+            try {
+                await signIn(email, password);
+                const isLoggedIn = client.authStore.isValid;
+                if (isLoggedIn) {
+                    navigate('/dashboard');
+                }
+            } catch (error) {
+                console.log(error.status)
+                setAlertMsg(`Error ${error.status} - ${error.message}`);
+                setOpenAlert(true);
+            }
+        } else {
+            setAlertMsg('Please fillout the form properly');
+            setOpenAlert(true);
+        }
+    };
+
     return (
+        <>
+        { client.authStore.isValid && (<Navigate to="/dashboard" replace={true} />)}
         <div className="w-full">
             <Card className="w-96 mx-auto">
                 <CardHeader
@@ -22,32 +68,50 @@ const SignIn = () => {
                         Sign In
                     </Typography>
                 </CardHeader>
-                <CardBody className="flex flex-col gap-4">
-                    <Input label="Email" size="lg" />
-                    <Input label="Password" size="lg" />
-                    <div className="-ml-2.5">
-                        <Checkbox label="Remember Me" />
-                    </div>
-                </CardBody>
-                <CardFooter className="pt-0">
-                    <Button variant="gradient" fullWidth>
-                        Sign In
-                    </Button>
-                    <Typography variant="small" className="mt-6 flex justify-center">
-                        Don&apos;t have an account?
-                        <Typography
-                            as="a"
-                            href="#signup"
-                            variant="small"
-                            color="blue-gray"
-                            className="ml-1 font-bold"
-                        >
-                            Sign up
-                        </Typography>
-                    </Typography>
-                </CardFooter>
+                <form onSubmit={handleSubmit}>
+                    <CardBody className="flex flex-col gap-4">
+
+                        <Input
+                            label="Email"
+                            type="email"
+                            name='email'
+                            onChange={handleInputChange}
+                            size="lg" />
+
+                        <Input
+                            label="Password"
+                            type="password"
+                            name='password'
+                            onChange={handleInputChange}
+                            size="lg" />
+
+                        {openAlert && (
+                            <Alert
+                                className="rounded-none border-l-4 border-[#c92e2e] bg-[#c92e2e]/10 font-medium text-[#c92e2e]"
+                            >
+                                {alertMsg}
+                            </Alert>
+                        )}
+
+                    </CardBody>
+                    <CardFooter className="pt-0">
+                        <Button
+                            variant="gradient"
+                            type='submit'
+                            fullWidth>
+                            Sign In
+                        </Button>
+                        <div className='flex gap-1 justify-center items-center mt-4'>
+                            <Typography variant="paragraph">
+                                Don't have an account?
+                            </Typography>
+                            <Link to='/signup'>Sign up</Link>
+                        </div>
+                    </CardFooter>
+                </form>
             </Card>
         </div>
+        </>
     )
 }
 
