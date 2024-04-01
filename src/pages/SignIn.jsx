273251b5ea97b4de666057dc00095f159bbb7add
sonictrain <nicola.brucoli92@gmail.com>
2024-04-01 23:28:54 +0100

    Added toaster and loading status. Refactored code to handle the api call and the related result inside the component

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -10,107 +10,111 @@ import {
     Input,
     Button,
 } from "@material-tailwind/react";
-import { client, signIn } from '../lib/pocketbase';
+import toast, { Toaster } from 'react-hot-toast';
+import { client, url } from '../lib/pocketbase';
 
 const SignIn = () => {
 
     const [userData, setUserData] = useState({
-        email: '',
+        identity: '',
         password: ''
     });
+    const [loading, setLoading] = useState(false);
+
     const navigate = useNavigate()
 
-    const [openAlert, setOpenAlert] = useState(false);
-    const [alertMsg, setAlertMsg] = useState('');
+    const [errorMsg, setErrorMsg] = useState({
+        identity: '',
+        password: '',
+    });
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
         setUserData({
             ...userData,
             [name]: value
-        })
+        });
+        setErrorMsg({
+            ...errorMsg,
+            [name]: ''
+        });
     };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setOpenAlert(false);
-        const { email, password } = userData;
+    const handleSignIn = async (e) => {
 
-        if (email.length > 0 && password.length > 0) {
-            try {
-                await signIn(email, password);
-                const isLoggedIn = client.authStore.isValid;
-                if (isLoggedIn) {
-                    navigate('/dashboard');
-                }
-            } catch (error) {
-                console.log(error.status)
-                setAlertMsg(`Error ${error.status} - ${error.message}`);
-                setOpenAlert(true);
-            }
-        } else {
-            setAlertMsg('Please fillout the form properly');
-            setOpenAlert(true);
-        }
+        e.preventDefault();
+        setLoading(true);
+
+        try {
+            const res = await client.collection("users").authWithPassword(userData.identity, userData.password);
+            if (res.token) {
+                setLoading(false);
+                navigate('/dashboard');
+            };
+        } catch (err) {
+            setLoading(false);
+            toast.error(`${err.message}`) 
+        };
     };
 
     return (
         <>
-        { client.authStore.isValid && (<Navigate to="/dashboard" replace={true} />)}
-        <div className="w-full">
-            <Card className="w-96 mx-auto">
-                <CardHeader
-                    variant="gradient"
-                    color="gray"
-                    className="mb-4 grid h-28 place-items-center"
-                >
-                    <Typography variant="h3" color="white">
-                        Sign In
-                    </Typography>
-                </CardHeader>
-                <form onSubmit={handleSubmit}>
-                    <CardBody className="flex flex-col gap-4">
-
-                        <Input
-                            label="Email"
-                            type="email"
-                            name='email'
-                            onChange={handleInputChange}
-                            size="lg" />
-
-                        <Input
-                            label="Password"
-                            type="password"
-                            name='password'
-                            onChange={handleInputChange}
-                            size="lg" />
-
-                        {openAlert && (
-                            <Alert
-                                className="rounded-none border-l-4 border-[#c92e2e] bg-[#c92e2e]/10 font-medium text-[#c92e2e]"
-                            >
-                                {alertMsg}
-                            </Alert>
-                        )}
-
-                    </CardBody>
-                    <CardFooter className="pt-0">
-                        <Button
-                            variant="gradient"
-                            type='submit'
-                            fullWidth>
+            <Toaster
+                position="top-right"
+                reverseOrder={false}
+                toastOptions={{
+                    duration: 5000
+                }}
+            />
+            {client.authStore.isValid && (<Navigate to="/dashboard" replace={true} />)}
+            <div className="w-full">
+                <Card className="w-96 mx-auto">
+                    <CardHeader
+                        variant="gradient"
+                        color="gray"
+                        className="mb-4 grid h-28 place-items-center"
+                    >
+                        <Typography variant="h3" color="white">
                             Sign In
-                        </Button>
-                        <div className='flex gap-1 justify-center items-center mt-4'>
-                            <Typography variant="paragraph">
-                                Don't have an account?
-                            </Typography>
-                            <Link to='/signup'>Sign up</Link>
-                        </div>
-                    </CardFooter>
-                </form>
-            </Card>
-        </div>
+                        </Typography>
+                    </CardHeader>
+                    <form onSubmit={handleSignIn}>
+                        <CardBody className="flex flex-col gap-4">
+
+                            <Input
+                                label="Email or username"
+                                type="text"
+                                name='identity'
+                                onChange={handleInputChange}
+                                size="lg" />
+
+                            <Input
+                                label="Password"
+                                type="password"
+                                name='password'
+                                onChange={handleInputChange}
+                                size="lg" />
+
+                        </CardBody>
+                        <CardFooter className="pt-0">
+                            <Button
+                                variant="gradient"
+                                type='submit'
+                                loading={loading}
+                                className='justify-center'
+                                fullWidth>
+                                Sign In
+                            </Button>
+                            <div className='flex gap-1 justify-center items-center mt-4'>
+                                <Typography variant="paragraph">
+                                    Don't have an account?
+                                </Typography>
+                                <Link to='/signup'>Sign up</Link>
+                            </div>
+                        </CardFooter>
+                    </form>
+                </Card>
+            </div>
         </>
     )
 }
