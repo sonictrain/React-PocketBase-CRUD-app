26b2ef62588c1f005edc4484cd0b22309782b656
sonictrain <nicola.brucoli92@gmail.com>
2024-04-02 23:54:55 +0100

    Implemented Avvvatars which creates a random shaped avatar based on the email

--- src/components/AvatarMenu.jsx
+++ src/components/AvatarMenu.jsx
@@ -4,14 +4,13 @@ import {
   MenuHandler,
   MenuList,
   MenuItem,
-  Button,
   Avatar,
   Typography,
 } from "@material-tailwind/react";
 import Avvvatars from 'avvvatars-react';
 import { signOut } from "../lib/pocketbase";
 
-const AvatarMenu = () => {
+const AvatarMenu = ({ email }) => {
   const navigate = useNavigate()
 
   const handleSignOut = () => {
@@ -23,13 +22,9 @@ const AvatarMenu = () => {
     <div className="fixed top-0 right-0 m-8 md:m-12 lg:m-20">
       <Menu>
         <MenuHandler>
-          {/* replace with Avvvatars */}
-          <Avatar
-            variant="circular"
-            alt="tania andrew"
-            className="cursor-pointer"
-            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
-          />
+          <div>
+          <Avvvatars value={ email } style="shape" shadow={false} size={48} />
+          </div>
         </MenuHandler>
 
         <MenuList>
