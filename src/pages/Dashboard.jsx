0aabf4e4f205d9d2f3ccf9250a4d24f6820f9fe9
sonictrain <nicola.brucoli92@gmail.com>
2024-04-02 23:55:30 +0100

    Fetching user email from authStore Model to generate the Avatar

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,20 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ToDoList from '../components/TodoList';
 import AddToDo from '../components/AddToDo';
 import AvatarMenu from '../components/AvatarMenu';
+import { client } from '../lib/pocketbase';
 
 const Dashboard = () => {
 
   const [keyData, setKeyData] = useState(0)
+  const [currentUserEmail, setCurrentUserEmail] = useState('');
 
   const incrementKey = () => {
     setKeyData(keyData + 1);
   }
 
+  useEffect(() => {
+    const getUserEmail = async () => {
+      const email = await client.authStore.model.email;
+      setCurrentUserEmail(email);
+    }
+
+    getUserEmail();
+  }, [])
+
   return (
     <>
         <ToDoList keyData={keyData} incrementKey={incrementKey}/>
-        <AvatarMenu />
+        <AvatarMenu email={currentUserEmail}/>
         <AddToDo incrementKey={incrementKey}/>
     </>
   )
