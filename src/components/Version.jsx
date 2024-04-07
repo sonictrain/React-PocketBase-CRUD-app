99ef678326bfe6b3ebf67eca3b22f8b31a3e087b
sonictrain <nicola.brucoli92@gmail.com>
2024-04-07 13:24:08 +0100

    Updating branch information

--- src/components/Version.jsx
+++ src/components/Version.jsx
@@ -5,11 +5,11 @@ const Version = () => {
         <Chip
             variant="ghost"
             className="fixed bottom-0 left-0 m-8 md:m-12 lg:m-20 w-max"
-            color="green"
+            color="amber"
             size="sm"
-            value="main"
+            value="refactor"
             icon={
-                <span className="mx-auto mt-1 block h-2 w-2 rounded-full bg-green-900 content-['']" />
+                <span className="mx-auto mt-1 block h-2 w-2 rounded-full bg-amber-900 content-['']" />
             }
         />
     )
