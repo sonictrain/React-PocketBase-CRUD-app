895036fa6f09d6454f57ccf0003fcc255dbc0633
sonictrain <nicola.brucoli92@gmail.com>
2024-04-08 20:31:06 +0100

    Added conditional rendering to prevent accessing the Dashboard functionalities if the email has not been verified

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,33 +1,48 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import { Alert, Button } from "@material-tailwind/react";
 import ToDoList from '../components/TodoList';
 import AddToDo from '../components/AddToDo';
 import AvatarMenu from '../components/AvatarMenu';
-import { client } from '../lib/pocketbase';
 
 const Dashboard = () => {
-
+  const isVerified = true;
+  const [open, setOpen] = useState(true);
   const [keyData, setKeyData] = useState(0)
-  const [currentUserEmail, setCurrentUserEmail] = useState('');
 
   const incrementKey = () => {
     setKeyData(keyData + 1);
   }
 
-  useEffect(() => {
-    const getUserEmail = async () => {
-      const email = await client.authStore.model.email;
-      setCurrentUserEmail(email);
-    }
-
-    getUserEmail();
-  }, [])
-
   return (
-    <>
-        <ToDoList keyData={keyData} incrementKey={incrementKey}/>
-        <AvatarMenu email={currentUserEmail}/>
-        <AddToDo incrementKey={incrementKey}/>
-    </>
+    (
+      <>
+        {isVerified ? (
+          <Alert
+            variant="gradient"
+            open={open}
+            action={
+              <Button
+                variant="text"
+                color="white"
+                size="sm"
+                className="!absolute top-3 right-3"
+                onClick={() => setOpen(false)}
+              >
+                Send verification
+              </Button>
+            }
+          >
+            Sorry your email doesn't appear to be verified yet. In order to use the system you have to verify your email.
+          </Alert>
+        ) : (
+          <>
+            <ToDoList keyData={keyData} incrementKey={incrementKey} />
+            <AvatarMenu email={currentUserEmail} />
+            <AddToDo incrementKey={incrementKey} />
+          </>
+        )}
+      </>
+    )
   )
 }
 
