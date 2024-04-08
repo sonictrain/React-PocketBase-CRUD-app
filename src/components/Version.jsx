1777794c2e4be2d4116dd6fcc1ca26850429ffad
sonictrain <nicola.brucoli92@gmail.com>
2024-04-08 19:57:32 +0100

    Updating branch information

--- src/components/Version.jsx
+++ src/components/Version.jsx
@@ -7,7 +7,7 @@ const Version = () => {
             className="fixed bottom-0 left-0 m-8 md:m-12 lg:m-20 w-max"
             color="amber"
             size="sm"
-            value="refactor"
+            value="new-features"
             icon={
                 <span className="mx-auto mt-1 block h-2 w-2 rounded-full bg-amber-900 content-['']" />
             }
