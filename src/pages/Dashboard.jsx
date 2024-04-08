9cfb77de8bda0880c91c05deed4a398af504cef5
sonictrain <nicola.brucoli92@gmail.com>
2024-04-08 22:12:24 +0100

    Fixed conditional rendering to show CTA to verified email before accessing the dashboard. Checking verification status using custom hook, and sending email verification link on click.

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,49 +1,60 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Alert, Button } from "@material-tailwind/react";
 import ToDoList from '../components/TodoList';
 import AddToDo from '../components/AddToDo';
 import AvatarMenu from '../components/AvatarMenu';
+import useVerified from '../hooks/useVerified';
+import { pb } from '../lib/pocketbase';
 
 const Dashboard = () => {
-  const isVerified = true;
+  const { isVerified, requestVerification } = useVerified();
   const [open, setOpen] = useState(true);
-  const [keyData, setKeyData] = useState(0)
+  const [keyData, setKeyData] = useState(0);
+  const [userEmail, setUserEmail] = useState('');
 
   const incrementKey = () => {
     setKeyData(keyData + 1);
   }
 
+  useEffect(() => {
+    const getUserEmail = async () => {
+      const email = await pb.authStore.model.email;
+      setUserEmail(email);
+    }
+
+    getUserEmail();
+  }, [])
+
   return (
-    (
-      <>
-        {isVerified ? (
-          <Alert
-            variant="gradient"
-            open={open}
-            action={
-              <Button
-                variant="text"
-                color="white"
-                size="sm"
-                className="!absolute top-3 right-3"
-                onClick={() => setOpen(false)}
-              >
-                Send verification
-              </Button>
-            }
-          >
-            Sorry your email doesn't appear to be verified yet. In order to use the system you have to verify your email.
-          </Alert>
-        ) : (
-          <>
-            <ToDoList keyData={keyData} incrementKey={incrementKey} />
-            <AvatarMenu email={currentUserEmail} />
-            <AddToDo incrementKey={incrementKey} />
-          </>
-        )}
-      </>
-    )
-  )
+    <>
+      {isVerified ? (
+        <>
+          <ToDoList keyData={keyData} incrementKey={incrementKey} email={userEmail} />
+          <AvatarMenu email={userEmail} />
+          <AddToDo incrementKey={incrementKey} />
+        </>
+      ) : (
+        <Alert
+          variant="gradient"
+          open={open}
+          className='flex flex-col md:flex-row'
+          action={
+            <Button
+              variant="text"
+              color="white"
+              size="sm"
+              className="ms-auto text-left"
+              onClick={requestVerification}
+            >
+              Send verification email
+            </Button>
+          }
+        >
+          Sorry your email doesn't appear to be verified yet. In order to use the system you have to verify your email.
+        </Alert>
+      )}
+    </>
+  );
 }
 
 export default Dashboard;
